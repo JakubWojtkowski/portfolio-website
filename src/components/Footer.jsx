615a4cadcb84b277f4eb27c996dd690e06407411
wojtko <j.w2001@onet.pl>
2023-05-14 23:07:35 +0200

    Minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,18 @@ function Footer() {
     <div className="footer">
       <h3>Copyright © wojtko. All rights are reserved 2023</h3>
       <div className="footer-socials">
-        <a target="_blank" rel="noreferrer" href="https://github.com/JakubWojtkowski">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/JakubWojtkowski"
+        >
           <i className="fa-brands fa-github footer-i"></i>
         </a>
-        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/jakub-wojtkowski-1aab37275/">
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/jakub-wojtkowski-1aab37275/"
+        >
           <i className="fa-brands fa-linkedin footer-i"></i>
         </a>
       </div>
