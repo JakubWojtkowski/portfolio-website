6f3eeb07fc8e4b8ca7ebc7f1c4ce59f9f099c779
wojtko <j.w2001@onet.pl>
2023-05-16 19:10:41 +0200

    Creating skill's card

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -25,14 +25,53 @@ function Skills() {
     <section id="skills">
       <div class="middle-container">
         <div className="skills">
-          <h2>SKILLS</h2>
-          <span>- An overview of my Technical Skills -</span>
+          <h2 >SKILLS</h2>
+          <span style={{paddingBottom: "20px"}}>- An overview of my Technical Skills -</span>
           <div className="skills-items">
             <div className="skill-item-card hidden">
               <h3>Front-end</h3>
+              <div className="skill-item-card-box">
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-js"></i>
+                  <p>Javascript</p>
+                  <span>Intermediate</span>
+                </div>
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-react"></i>
+                  <p>React.js</p>
+                  <span>Basic</span>
+                </div>
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-html5"></i>
+                  <p>HTML</p>
+                  <span>Intermediate</span>
+                </div>
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-css3-alt"></i>
+                  <p>CSS</p>
+                  <span>Intermediate</span>
+                </div>
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-bootstrap"></i>
+                  <p>Bootstrap</p>
+                  <span>Basic</span>
+                </div>
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-git-alt"></i>
+                  <p>Git</p>
+                  <span>Basic</span>
+                </div>
+              </div>
             </div>
             <div className="skill-item-card hidden">
               <h3>Back-end</h3>
+              <div className="skill-item-card-box">
+                <div className="skill-item-card-box-element">
+                  <i class="fa-brands fa-node"></i>
+                  <p>Node.js</p>
+                  <span>Intermediate</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
