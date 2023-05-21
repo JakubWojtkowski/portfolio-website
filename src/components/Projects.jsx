7fb9b6193fb7b5ed1b863ed8db419eb86d030a6c
wojtko <j.w2001@onet.pl>
2023-05-21 18:15:24 +0200

    Creating projects part

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,19 +8,49 @@ function Projects() {
         <div className="projects">
           <h2>PROJECTS</h2>
           <div className="projects-item">
-            <div className="project-item-main">
-              <div className="project-item-main-text">
-                <h3>Project 1</h3>
-                <p>Lorem ipsum ...</p>
-                <a href="https://github.com/JakubWojtkowski">
-                  See code <i className="fa-brands fa-github"></i>
-                </a>
-              </div>
+            <div className="project-item-element">
               <div className="project-item-main-img">
                 <img
                   src="https://images.pexels.com/photos/16849712/pexels-photo-16849712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   alt="temp"
                 />
+              </div>
+              <div className="project-item-main-text">
+                <h3>This is project card</h3>
+                <a href="https://github.com/JakubWojtkowski">
+                  See code <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="project-item-element">
+              <div className="project-item-main-img">
+                <img
+                  src="https://images.pexels.com/photos/16849712/pexels-photo-16849712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="temp"
+                />
+              </div>
+              <div className="project-item-main-text">
+                <h3>This is project card</h3>
+
+                <a href="https://github.com/JakubWojtkowski">
+                  See code <i className="fa-brands fa-github"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="project-item-element">
+              <div className="project-item-main-img">
+                <img
+                  src="https://images.pexels.com/photos/16849712/pexels-photo-16849712.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt="temp"
+                />
+              </div>
+              <div className="project-item-main-text">
+                <h3>This is project card</h3>
+                <a href="https://github.com/JakubWojtkowski">
+                  See code <i className="fa-brands fa-github"></i>
+                </a>
               </div>
             </div>
           </div>
