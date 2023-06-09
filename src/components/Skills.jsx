d9626155eae87eeb273b1814a05132a861bf8bb7
wojtko <j.w2001@onet.pl>
2023-06-10 00:28:52 +0200

    Make intro-img animated back. Improve styles

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -19,7 +19,6 @@ function Skills() {
   function showSkills() {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
-        console.log(entry);
         if (entry.isIntersecting) {
           entry.target.classList.add("show");
         } else {
